43e446b108066d79a64ea485edf5afcb6a423436
simonedemutiis <78730506+simonedemutiis@users.noreply.github.com>
2021-05-11 21:20:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function addBudget() {
+  input = document.getElementById("enter").value;
+  document.getElementById("bud").innerHTML = input;
+  document.getElementById("bal").innerHTML = input;
+}
+
+function addExpenses() {
+  var tot;
+  var totale;
+  var exp1 = document.getElementById("addExp").value;
+  var exp2 = document.getElementById("addBal").value;
+
+  tot = exp1 - exp2;
+
+  addExpenses = document.getElementById("addBal").value;
+  document.getElementById("exp").innerHTML = addExpenses;
+
+  totale = input - tot;
+
+  document.getElementById("bal").innerHTML = totale;
+}
+
+function addExp() {
+  var exp = document.getElementById("addExp").value;
+  document.getElementById("expense2").innerHTML = exp;
+}
